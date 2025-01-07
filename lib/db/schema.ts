456662ece90314b9ccaf0e5287794bd1641295a0
tomerlevi444 <tomerlevi444@users.noreply.github.com>
2025-01-07 12:55:04 +0200

    revert schema change for Chat

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -28,7 +28,9 @@ export const chat = pgTable('Chat', {
   id: uuid('id').primaryKey().notNull().defaultRandom(),
   createdAt: timestamp('createdAt').notNull(),
   title: text('title').notNull(),
-  userId: uuid('userId'),
+  userId: uuid('userId')
+    .notNull()
+    .references(() => user.id),
   visibility: varchar('visibility', { enum: ['public', 'private'] })
     .notNull()
     .default('private'),
